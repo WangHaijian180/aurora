b66350cfc3a4af4d12a8250f4a80f8ec002ff686
whj <1469202826@qq.com>
2021-02-04 14:25:00 +0800

    123

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="index">
-    <div class="index_image"></div>
   </div>
 </template>
 
@@ -15,25 +14,5 @@ export default {
 }
 </script>
 <style scoped>
-.index {
-  /* background-image: url("../../../static/img/769e693be870938c8866435324efc6d9.jpg"); */
-  background-image: url("../../../static/img/aHR0cDovL3d3dy56aHVveWFuZ2xpdS5vcmcvcWZ5LWNvbnRlbnQvdXBsb2Fkcy8yMDIwLzAzLzM5NzRkZDMzNTUxZDQ2NTRiMzE0NWMyOTk4Y2U5OGRhLmdpZg_p_p100_p_3D_p_p100_p_3D.m.gif");
-  position: absolute;
-  background-size: cover;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  top: 0;
-}
-.index_image {
-  /* background-image: url("../../../static/img/aHR0cDovL3d3dy56aHVveWFuZ2xpdS5vcmcvcWZ5LWNvbnRlbnQvdXBsb2Fkcy8yMDIwLzAzLzM5NzRkZDMzNTUxZDQ2NTRiMzE0NWMyOTk4Y2U5OGRhLmdpZg_p_p100_p_3D_p_p100_p_3D.m.gif"); */
-  width: 1200px;
-  height: 100%;
-  margin: 0 auto;
-}
-img {
-  width: 120px;
-  height: 120px;
-  border-radius: 50%;
-}
+
 </style>
